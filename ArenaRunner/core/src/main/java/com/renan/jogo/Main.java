d2075ee3301f2ae94e772d143d0695437376784f
idknHB <renanhammerschlagbueno@gmail.com>
2026-06-01 17:02:58 -0300

    Fix project structure and update repository
    Reorganized project files and synchronized the latest changes with the repository

--- ArenaRunner/core/src/main/java/com/renan/jogo/Main.java
+++ ArenaRunner/core/src/main/java/com/renan/jogo/Main.java
@@ -12,14 +12,19 @@ public class Main extends ApplicationAdapter {
     private OrthographicCamera camera;
     private ShapeRenderer shape;
 
-    private float x = 0;
-    private float y = 0;
-    private float speed = 200;
+    Player player;
+    Enemy enemy;
+
+    private float deathTimer = 0;
+    boolean isGameOver = false;
 
     @Override
     public void create() {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 600);
+
+        player = new Player(0, 0);
+        enemy = new Enemy(400, 300);
 
         shape = new ShapeRenderer();
     }
@@ -28,21 +33,45 @@ public class Main extends ApplicationAdapter {
     public void render() {
         float delta = Gdx.graphics.getDeltaTime();
 
-        // Movimento
-        if (Gdx.input.isKeyPressed(Input.Keys.W)) y += speed * delta;
-        if (Gdx.input.isKeyPressed(Input.Keys.S)) y -= speed * delta;
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) x -= speed * delta;
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) x += speed * delta;
+        player.update(delta);
+        enemy.update(delta, player);
 
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         shape.setProjectionMatrix(camera.combined);
-
         shape.begin(ShapeRenderer.ShapeType.Filled);
-        shape.rect(x, y, 50, 50); // player (quadrado)
+
+        player.draw(shape);
+        enemy.draw(shape);
+
         shape.end();
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.E) && player.isAlive) {
+            player.attack(enemy);
+        }
+
+        if (enemy.isAlive) {
+            enemy.update(delta, player);
+        }
+
+        if (!player.isAlive && !isGameOver) {
+            isGameOver = true;
+            deathTimer = 2f; // 2 segundos
+        }
+        if (isGameOver) {
+            deathTimer -= delta;
+
+            if (deathTimer <= 0) {
+                resetGame();
+                isGameOver = false;
+            }
+        }
     }
 
+    private void resetGame(){
+        player = new Player(0,0);
+        enemy = new Enemy(400,300);
+    }
     @Override
     public void dispose() {
         shape.dispose();
